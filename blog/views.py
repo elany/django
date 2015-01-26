1d974d42f548bc595883452993c544244b7ac995
elany <elany27@hotmail.com>
2015-01-26 21:31:30 +0100

    css

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,4 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+    
