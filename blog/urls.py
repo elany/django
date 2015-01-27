c4b2febd4fb4d4648e764dd0c4ad4f2c9d3d9331
elany <elany27@hotmail.com>
2015-01-27 13:25:13 +0100

    pagespagespages

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
 )
+
+def post_detail(request, pk):
+	Post.objects.get_object_or_404(pk=pk)
